588ff36caa83d327742934fb705054f7e200aeb2
Sachinrathee2505 <148993586+Sachinrathee2505@users.noreply.github.com>
2025-09-13 05:54:42 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,27 @@
-const CACHE_NAME = 'placement-command-center-v1';
+const CACHE_NAME = 'placement-command-center-v2'; // Updated version
 const URLS_TO_CACHE = [
-  '/',
-  '/index.html'
+  './', // This is the main page (index.html)
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install the service worker and cache the files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
+        console.log('Opened cache and caching files');
         return cache.addAll(URLS_TO_CACHE);
       })
   );
 });
 
-// Serve cached content when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+        return response || fetch(event.request);
+      })
   );
 });
