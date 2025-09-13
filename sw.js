4cf0da23500a7e1d20252b5ee10692553db1b63a
Sachinrathee2505 <148993586+Sachinrathee2505@users.noreply.github.com>
2025-09-13 17:52:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,49 @@
-const CACHE_NAME = 'placement-command-center-v2'; // Updated version
+// The version number is updated to trigger a new install
+const CACHE_NAME = 'placement-command-center-v3'; 
+
 const URLS_TO_CACHE = [
-  './', // This is the main page (index.html)
+  './', 
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  'https://cdn.tailwindcss.com' // <-- This is the new line we added
 ];
 
+// Install the service worker and cache the files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache and caching files');
+        console.log('Opened cache and caching all assets');
         return cache.addAll(URLS_TO_CACHE);
       })
   );
 });
 
+// Serve cached content when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Return the cached version if found, otherwise fetch from the network
         return response || fetch(event.request);
       })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
